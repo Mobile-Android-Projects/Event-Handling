94d1f76010d4960fd8f0572f65ff6524cb8e3e8b
Auravision <auravision.africa@gmail.com>
2021-10-19 10:16:03 +0300

    Done with a very basic event handling app

--- app/src/main/java/com/example/eventhandling/MainActivity.kt
+++ app/src/main/java/com/example/eventhandling/MainActivity.kt
@@ -2,10 +2,28 @@ package com.example.eventhandling
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.example.eventhandling.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val root = binding.root
+        setContentView(root)
+
+        binding.myButton.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                binding.statusText.text = getString(R.string.button_pressed)
+            }
+        })
+
+        binding.myButton.setOnLongClickListener {
+            binding.statusText.text = getString(R.string.long_pressed)
+            false
+        }
     }
 }
